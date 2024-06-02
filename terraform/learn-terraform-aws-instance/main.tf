6ddf239802edf656723fd7e3a457fea8b15388de
Anthony Burdi <anthonyburdi@gmail.com>
2024-06-02 17:18:31 -0400

    AWS getting started tutorial

---
+++ terraform/learn-terraform-aws-instance/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+# Provider specific configuration
+provider "aws" {
+  region  = "us-west-2"
+}
+
+# Components of the infrastructure
+# resource "<type>" "<name>"
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
