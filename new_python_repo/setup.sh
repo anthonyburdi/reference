bf1caa461eaab7acade7221e9f31efe0e3108673
Anthony Burdi <anthonyburdi@gmail.com>
2024-09-23 16:31:18 -0400

    Update setup steps in setup script

--- new_python_repo/setup.sh
+++ new_python_repo/setup.sh
@@ -2,5 +2,15 @@
 
 source logging.sh
 
+print_orange_header "Installing pyenv & python 3.11"
+brew install pyenv
+pyenv install 3.11
+
 print_orange_header "Installing pipx"
 brew install pipx
+pipx ensurepath
+
+print_orange_header "Installing poetry"
+pipx install poetry
+
+print_orange_header "You are set up globally. Now follow the Repo Specific instructions."
